958a45b72555a859967ab19465f90ad79a03d056
Gusti Cahya <gusticahyax@yahoo.com>
2021-11-18 17:49:52 +0800

    yess

--- a.py
+++ a.py
@@ -3,10 +3,10 @@
 import json
 import unicodecsv as csv
 
-with open('data.json','rb') as fin:
+with open('Absensi.json','rb') as fin:
     data = json.load(fin)
 
-with open('data.csv','wb') as csv_file:
+with open('Absensi.csv','wb') as csv_file:
     w = csv.writer(csv_file, encoding='utf-8')
     w.writerow(data[0].keys())  # header row
     for row in data:
